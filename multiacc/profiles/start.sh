c915c3d97ea126c6fe399bfeb1dc00d3a654157b
bussas <bussas60@gmail.com>
2017-06-15 18:01:21 +0700

    "Для человеческой глупости патча нет"

--- multiacc/profiles/start.sh
+++ multiacc/profiles/start.sh
@@ -2,23 +2,23 @@
 
 screen -S cli1 -dm ./telegram-cli --json -p profile_1 -P 2001
 
-screen -S cli2 -dm ./telegram-cli -p profile_1 --json -P 2002
+screen -S cli2 -dm ./telegram-cli -p profile_2 --json -P 2002
 
-screen -S cli3 -dm ./telegram-cli -p profile_1 --json -P 2003
+screen -S cli3 -dm ./telegram-cli -p profile_3 --json -P 2003
 
-screen -S cli4 -dm ./telegram-cli -p profile_1 --json -P 2004
+screen -S cli4 -dm ./telegram-cli -p profile_4 --json -P 2004
 
-screen -S cli5 -dm ./telegram-cli -p profile_1 --json -P 2005
+screen -S cli5 -dm ./telegram-cli -p profile_5 --json -P 2005
 
-screen -S cli6 -dm ./telegram-cli -p profile_1 --json -P 2006
+screen -S cli6 -dm ./telegram-cli -p profile_6 --json -P 2006
 
-screen -S cli7 -dm ./telegram-cli -p profile_1 --json -P 2007
+screen -S cli7 -dm ./telegram-cli -p profile_7 --json -P 2007
 
-screen -S cli8 -dm ./telegram-cli -p profile_1 --json -P 2008
+screen -S cli8 -dm ./telegram-cli -p profile_8 --json -P 2008
 
-screen -S cli9 -dm ./telegram-cli -p profile_1 --json -P 2009
+screen -S cli9 -dm ./telegram-cli -p profile_9 --json -P 2009
 
-screen -S cli10 -dm ./telegram-cli -p profile_1 --json -P 2010
+screen -S cli10 -dm ./telegram-cli -p profile_10 --json -P 2010
 
 screen -S bot1 -dm python3 ./main.py --admin $1 --order $1 --castle mint --gold $2 --buy 1 --lvlup lvl_def --group_name $3 -p 2001
 
