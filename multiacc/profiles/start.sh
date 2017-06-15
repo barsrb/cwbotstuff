a7ee8fa5ac01565e50063bbed6df16fcab947186
bussas <bussas60@gmail.com>
2017-06-15 16:32:14 +0700

    Какая-то хуйня, пытаюсь фиксить

--- multiacc/profiles/start.sh
+++ multiacc/profiles/start.sh
@@ -20,22 +20,22 @@ screen -S cli9 -dm ./telegram-cli -p profile_1 --json -P 2009
 
 screen -S cli10 -dm ./telegram-cli -p profile_1 --json -P 2010
 
-screen -S bot1 -dm python3 ./main.py --admin $1 --order $1 --castle mint --gold $2 --buy 1 --lvlup lvl_def --group_name $3 --port 2001
+screen -S bot1 -dm python3 ./main.py --admin $1 --order $1 --castle mint --gold $2 --buy 1 --lvlup lvl_def --group_name $3 -p 2001
 
-screen -S bot2 -dm python3 ./main.py --admin $1 --order $1 --castle twilight --gold $2 --buy 1 --lvlup lvl_def --group_name $3 --port 2002
+screen -S bot2 -dm python3 ./main.py --admin $1 --order $1 --castle twilight --gold $2 --buy 1 --lvlup lvl_def --group_name $3 -p 2002
 
-screen -S bot3 -dm python3 ./main.py --admin $1 --order $1 --castle yellow --gold $2 --buy 1 --lvlup lvl_def --group_name $3 --port 2003
+screen -S bot3 -dm python3 ./main.py --admin $1 --order $1 --castle yellow --gold $2 --buy 1 --lvlup lvl_def --group_name $3 -p 2003
 
-screen -S bot4 -dm python3 ./main.py --admin $1 --order $1 --castle white --gold $2 --buy 1 --lvlup lvl_def --group_name $3 --port 2004
+screen -S bot4 -dm python3 ./main.py --admin $1 --order $1 --castle white --gold $2 --buy 1 --lvlup lvl_def --group_name $3 -p 2004
 
-screen -S bot5 -dm python3 ./main.py --admin $1 --order $1 --castle white --gold $2 --buy 1 --lvlup lvl_def --group_name $3 --port 2005
+screen -S bot5 -dm python3 ./main.py --admin $1 --order $1 --castle white --gold $2 --buy 1 --lvlup lvl_def --group_name $3 -p 2005
 
-screen -S bot6 -dm python3 ./main.py --admin $1 --order $1 --castle red --gold $2 --buy 1 --lvlup lvl_def --group_name $3 --port 2006
+screen -S bot6 -dm python3 ./main.py --admin $1 --order $1 --castle red --gold $2 --buy 1 --lvlup lvl_def --group_name $3 -p 2006
 
-screen -S bot7 -dm python3 ./main.py --admin $1 --order $1 --castle blue --gold $2 --buy 1 --lvlup lvl_def --group_name $3 --port 2007
+screen -S bot7 -dm python3 ./main.py --admin $1 --order $1 --castle blue --gold $2 --buy 1 --lvlup lvl_def --group_name $3 -p 2007
 
-screen -S bot8 -dm python3 ./main.py --admin $1 --order $1 --castle black --gold $2 --buy 1 --lvlup lvl_def --group_name $3 --port 2008
+screen -S bot8 -dm python3 ./main.py --admin $1 --order $1 --castle black --gold $2 --buy 1 --lvlup lvl_def --group_name $3 -p 2008
 
-screen -S bot9 -dm python3 ./main.py --admin $1 --order $1 --castle white --gold $2 --buy 1 --lvlup lvl_def --group_name $3 --port 2009
+screen -S bot9 -dm python3 ./main.py --admin $1 --order $1 --castle white --gold $2 --buy 1 --lvlup lvl_def --group_name $3 -p 2009
 
-screen -S bot10 -dm python3 ./main.py --admin $1 --order $1 --castle mint --gold $2 --buy 1 --lvlup lvl_def --group_name $3 --port 2010
+screen -S bot10 -dm python3 ./main.py --admin $1 --order $1 --castle mint --gold $2 --buy 1 --lvlup lvl_def --group_name $3 -p 2010
